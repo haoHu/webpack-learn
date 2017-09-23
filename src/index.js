acb93ca9b3acb21d7d1e729c3b16da8aee5a71aa
huhao <jackeysun1818@gmail.com>
2017-09-23 16:49:28 +0800

    图片资源管理

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './images/myicon.png';
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['hello', 'world'], ' ');
 
     element.classList.add('hello');
+    // 将图像添加到现有的div中
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
     return element;
 }
 
